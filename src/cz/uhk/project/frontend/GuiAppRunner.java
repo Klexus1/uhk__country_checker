d8415f5b3c589125fe3067f70f3ed9955c62294d
Klexus1 <47198827+Klexus1@users.noreply.github.com>
2022-02-18 12:50:25 +0100

    add buttons

--- src/cz/uhk/project/frontend/GuiAppRunner.java
+++ src/cz/uhk/project/frontend/GuiAppRunner.java
@@ -5,6 +5,12 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 
 public class GuiAppRunner extends JFrame {
+    JButton buttonList = new JButton("Zobrazit seznam");
+    JButton buttonCreate = new JButton("Přidat záznam");
+    JButton buttonUpdate = new JButton("Aktualizovat záznam");
+    JButton buttonDelete = new JButton("Odstranit záznam");
+
+    JPanel panel = new JPanel();
 
     public static void main(String[] args) {
         new GuiAppRunner().setVisible(true);
@@ -16,7 +22,17 @@ public class GuiAppRunner extends JFrame {
     public GuiAppRunner(){
         super("COUNTRY CHECKER");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(500, 500);
+        setSize(800, 500);
         setBackground(Color.LIGHT_GRAY);
+
+        this.getContentPane().add(panel);
+        addButtons(panel);
+    }
+
+    private void addButtons(JPanel panel){
+        panel.add(buttonList);
+        panel.add(buttonCreate);
+        panel.add(buttonUpdate);
+        panel.add(buttonDelete);
     }
 }
